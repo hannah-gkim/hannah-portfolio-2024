c4b37547fd4a35b7dc8e6928ebbf52a73e5915ed
Hannah G Kim <hannahgomezkim@gmail.com>
2024-10-25 13:57:25 +0900

    data added, package updaed

---
+++ src/app/data.js
@@ -0,0 +1,41 @@
+export const projectsData = [
+  {
+    id: 1,
+    name: "Portfolio ver. 2",
+    description: "Hannah's portfolio version 2",
+    demoLink: "https://hannahgkim.com/",
+  },
+  {
+    id: 1,
+    name: "Frame by Frame",
+    description: "A photographer's portfolio web app",
+    demoLink:
+      "https://github.com/hannah-gkim/porfolioGallery-snowflakes-firebase?tab=readme-ov-file",
+  },
+  {
+    id: 2,
+    name: "Ghibli Movies Filter App",
+    description: "An movie filtering app using the Ghibli API and Vue.js",
+    date: "2022-06-20",
+    demoLink: "https://hannah-gkim.github.io/vue-js-Ghibli/",
+  },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://www.github.com/hannah-gkim",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/hannah-gkim",
+    icon: "linkedin",
+    newTab: true,
+  },
+];
