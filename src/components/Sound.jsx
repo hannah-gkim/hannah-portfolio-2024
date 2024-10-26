8020a2d59b825f72b50d9a80286b2a8e579952a4
Hannah G Kim <hannahgomezkim@gmail.com>
2024-10-26 14:12:43 +0900

    audio fix

--- src/components/Sound.jsx
+++ src/components/Sound.jsx
@@ -87,7 +87,7 @@ const Sound = () => {
       )}
 
       <audio ref={audioRef} loop>
-        <source src={"/audio/mysterious-grove.mp3"} type="audio/mpeg" />
+        <source src={"./audio/mysterious-grove.mp3"} type="audio/mpeg" />
         your browser does not support the audio element.
       </audio>
       <motion.button
