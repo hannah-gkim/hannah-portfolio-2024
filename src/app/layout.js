21d70ff10eb3f3ed899ed23332d2dcc2a9c84fce
Hannah G Kim <hannahgomezkim@gmail.com>
2024-10-26 16:56:49 +0900

    preview image try 1

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,18 +18,31 @@ const poppins = Poppins({
 
 export const metadata = {
   title: {
-    template: "Hannah's portfolio 2024",
-    default: "Hannah's portfolio 2024",
+    template: "Hannah Gomez Kim",
+    default: "Hannah Gomez Kim",
   },
-  description: "Hannah's portfolio 2024",
+  description: "Hannah Gomez Kim",
+  openGraph: {
+    title: "Hannah Gomez Kim",
+    description: "portfolio for 2024",
+    images: [
+      {
+        url: "./preview/preview-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Hannah's Portfolio Preview",
+      },
+    ],
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
+      {/* <Head>
         <meta property="og:image" content="./preview/preview-image.png" />
-      </Head>
+      </Head> */}
       <body
         className={clsx(
           poppins.variable,
