415a8e33dd01b0209c5ecb3f37959ae3426d3409
Hannah G Kim <hannahgomezkim@gmail.com>
2024-10-28 10:02:46 +0900

    preview fix 1

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,27 +22,27 @@ export const metadata = {
     default: "Hannah Gomez Kim",
   },
   description: "Hannah Gomez Kim",
-  openGraph: {
-    title: "Hannah Gomez Kim",
-    description: "portfolio for 2024",
-    images: [
-      {
-        url: "./preview/preview-image.png",
-        width: 1200,
-        height: 630,
-        alt: "Hannah's Portfolio Preview",
-      },
-    ],
-    type: "website",
-  },
+  // openGraph: {
+  //   title: "Hannah Gomez Kim",
+  //   description: "portfolio for 2024",
+  //   images: [
+  //     {
+  //       url: "./preview/preview-image.png",
+  //       width: 1200,
+  //       height: 630,
+  //       alt: "Hannah's Portfolio Preview",
+  //     },
+  //   ],
+  //   type: "website",
+  // },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <Head>
+      <Head>
         <meta property="og:image" content="./preview/preview-image.png" />
-      </Head> */}
+      </Head>
       <body
         className={clsx(
           poppins.variable,
