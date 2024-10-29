416a193a110d651efb986d32bf7961183b6de43b
Hannah G Kim <hannahgomezkim@gmail.com>
2024-10-29 12:15:01 +0900

    og image try 1

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter, Poppins } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 import clsx from "clsx";
 import FireFliesBackground from "@/components/FireFliesBackground";
@@ -21,24 +22,14 @@ export const metadata = {
     default: "Hannah Gomez Kim",
   },
   description: "Hannah Gomez Kim",
-  openGraph: {
-    title: "Hannah Gomez Kim",
-    description: "portfolio for 2024",
-    images: [
-      {
-        url: "preview-image.png",
-        width: 1200,
-        height: 630,
-        alt: "Hannah's Portfolio Preview",
-      },
-    ],
-    type: "website",
-  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta property="og:image" content="./preview/preview-image.png" />
+      </Head>
       <body
         className={clsx(
           poppins.variable,
